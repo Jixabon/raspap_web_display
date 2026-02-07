c908a63fa758f12c99940289336a72ed1c5476bb
Shawn Holmes <sherlock656@gmail.com>
2026-02-07 09:33:39 -0700

    Add clean up function to strip output of extra characters

--- fallbacks.js
+++ fallbacks.js
@@ -2,33 +2,42 @@ import util from 'node:util';
 import child_process from 'child_process';
 const exec = util.promisify(child_process.exec);
 
+export function cleanOutput(string) {
+    string = string.replace(/[\n\r]/g, '');
+    string = string.trim();
+
+    return string;
+}
+
 export async function getRaspAPVersion() {
-    exec("grep 'RASPI_VERSION' /var/www/html/includes/defaults.php | awk -F\"'\" '{print $4}'")
-        .then((stdout) => {
-            return stdout;
-        })
-        .catch((e) => {
-            console.error(e);
-        });
+    const {stdout, stderr} = await exec("grep 'RASPI_VERSION' /var/www/html/includes/defaults.php | awk -F\"'\" '{print $4}'");
+    
+    if (stderr) {
+        console.error(stderr);
+        return;
+    }
+
+    return cleanOutput(stdout);
 }
 
 export async function getAPInterface() {
-    exec("cat /etc/hostapd/hostapd.conf | grep '^interface=' | cut -d'=' -f2")
-        .then((stdout) => {
-            return stdout;
-        })
-        .catch((e) => {
-            console.error(e);
-        });
+    const {stdout, stderr} = await exec("cat /etc/hostapd/hostapd.conf | grep '^interface=' | cut -d'=' -f2");
+
+    if (stderr) {
+        console.error(stderr);
+        return;
+    }
+
+    return cleanOutput(stdout);
 }
 
 export async function getWPAPassphrase() {
-    exec("sed -En 's/wpa_passphrase=(.*)/\\1/p' /etc/hostapd/hostapd.conf")
-        .then((stdout) => {
-            return stdout;
-        })
-        .catch((e) => {
-            console.error(e);
-            return null;
-        });
+    const {stdout, stderr} = await exec("sed -En 's/wpa_passphrase=(.*)/\\1/p' /etc/hostapd/hostapd.conf");
+    
+    if (stderr) {
+        console.error(stderr);
+        return;
+    }
+
+    return cleanOutput(stdout);
 }
