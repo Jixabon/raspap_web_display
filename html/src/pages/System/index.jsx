ec8dfc06634e10b01cc84042ab1bd609c3486a0e
Shawn Holmes <sherlock656@gmail.com>
2026-02-07 16:44:26 -0700

    finish implementing metrics and other styling

--- html/src/pages/System/index.jsx
+++ html/src/pages/System/index.jsx
@@ -5,19 +5,19 @@ import Footer from '../../components/Footer';
 import Overlay from '../../components/Overlay';
 import RaspAPLogo from "../../components/RaspAPLogo";
 import { SHORT_POLLING_INTERVAL } from "../../config";
+import Metric from "../../components/Metric";
 
 export function System() {
     const [data, setData] = useState(null);
     const [isLoading, setIsLoading] = useState(true);
     const [error, setError] = useState(null);
 
-    const [showAdminQR, setShowAdminQR] = useState(false);
-
     const default_data = {
         hostname: "raspap",
         uptime: "up 2 days, 4 hours, 31 minutes",
         systime: "Fri  6 Feb 23:32:26 MST 2026",
         usedMemory: 19.88,
+        useDisk: 0,
         processorCount: 4,
         LoadAvg1Min: 0,
         systemLoadPercentage: 0,
@@ -26,18 +26,16 @@ export function System() {
         kernelVersion: "6.12.62+rpt-rpi-2712",
         rpiRevision: "Pi 5 (8 GB)",
         raspapVersion: '3.5.2',
-        networking: {
-            interfaces: {
-                eth0: {
-                    IP_address: "192.168.1.25",
-                    Netmask: "255.255.255.0",
-                    MAC_address: "2c:cf:67:52:8e:b0"
-                },
-                wlan0: {
-                    IP_address: "10.3.141.1",
-                    Netmask: "255.255.255.0",
-                    MAC_address: "2c:cf:67:52:8e:b3"
-                }
+        interfaces: {
+            eth0: {
+                IP_address: "192.168.1.25",
+                Netmask: "255.255.255.0",
+                MAC_address: "2c:cf:67:52:8e:b0"
+            },
+            wlan0: {
+                IP_address: "10.3.141.1",
+                Netmask: "255.255.255.0",
+                MAC_address: "2c:cf:67:52:8e:b3"
             }
         }
     };
@@ -74,7 +72,10 @@ export function System() {
 
     if (isLoading) {
         return <Overlay show={true}>
-            <span>Loading...</span>
+            <div className="flex flex-col items-center gap-6">
+                <RaspAPLogo style={{width: 150, height: 150}} animate={true}/>
+                <span className="text-3xl font-bold">Loading...</span>
+            </div>
         </Overlay>
     }
 
@@ -87,30 +88,37 @@ export function System() {
                     <span className="text-4xl font-bold text-teal">RaspAP</span>
                     <span>v{data.raspapVersion}</span>
                 </div>
-                <div className="grid grid-cols-2 gap-y-2 mb-8">
-                    <span className="font-bold">Hardware</span>
+                <div className="grid grid-cols-[min-content_1fr] gap-y-2 mb-8">
+                    <span className="font-bold whitespace-nowrap pr-6">Hardware</span>
                     <span>{data.rpiRevision}</span>
-                    <span className="font-bold">Hostname</span>
+                    <span className="font-bold whitespace-nowrap pr-6">Hostname</span>
                     <span>{data.hostname}</span>
-                    <span className="font-bold">Operating System</span>
+                    <span className="font-bold whitespace-nowrap pr-6">Operating System</span>
                     <span>{data.operatingSystem}</span>
-                    <span className="font-bold">Kernel Version</span>
+                    <span className="font-bold whitespace-nowrap pr-6">Kernel Version</span>
                     <span>{data.kernelVersion}</span>
-                    <span className="font-bold">System Time</span>
+                    <span className="font-bold whitespace-nowrap pr-6">System Time</span>
                     <span>{data.systime}</span>
-                    <span className="font-bold">Uptime</span>
+                    <span className="font-bold whitespace-nowrap pr-6">Uptime</span>
                     <span>{data.uptime}</span>
                 </div>
                 <div className="mb-8">
-                    <div className="relative flex justify-center bg-white rounded-lg h-6">
-                        <div className="absolute top-0 left-0 bg-blue-200 rounded-lg h-6" style={{width: '50%'}}></div>
-                        <span className="text-black z-10">50%</span>
-                    </div>
+                    {/*
+                    processorCount: 4,
+                    systemTemperature: 56.2, */}
+                    <span className="font-bold">Memory Used</span>
+                    <Metric value={data.usedMemory} suffix="%" className="mb-3"/>
+                    <span className="font-bold">Disk Used</span>
+                    <Metric value={data.usedDisk} suffix="%" className="mb-3"/>
+                    <div className="flex gap-2"><span className="font-bold">CPU Load</span><span>({data.LoadAvg1Min}% 1min avg)</span></div>
+                    <Metric value={data.systemLoadPercentage} suffix="%" className="mb-3"/>
+                    <span className="font-bold">CPU Temp</span>
+                    <Metric value={data.systemTemperature} suffix="C" className="mb-3"/>
                 </div>
                 <div>
                     <details className="border rounded">
                         <summary className="rounded px-4 py-2 text-3xl">Interfaces</summary>
-                        {Object.entries(data?.networking?.interfaces).map(([key, item]) => {
+                        {Object.entries(data?.interfaces).map(([key, item]) => {
                             return (
                                 <div className="mt-5 px-4 last:mb-5">
                                     <h3 className="w-full text-2xl font-bold mb-3 border-b">{key}</h3>
