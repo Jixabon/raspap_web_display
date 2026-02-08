c6164075c3dc40de29c183aac762fc41d9193303
Shawn Holmes <sherlock656@gmail.com>
2026-02-08 11:58:00 -0700

    theming

--- html/src/components/Overlay.jsx
+++ html/src/components/Overlay.jsx
@@ -4,7 +4,7 @@ export default function Overlay({ show, children }) {
     return (
         <div className="fixed inset-0 z-50 flex items-center justify-center">
             <div
-                className="absolute inset-0 bg-black/50 backdrop-blur-sm"
+                className="absolute inset-0 bg-white/50 dark:bg-black/50 backdrop-blur-sm"
                 // onClick={onClose}
             />
 
