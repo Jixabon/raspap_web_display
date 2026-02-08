0a15b54710131e23340909184f4ecdcf9ade9f8e
Shawn Holmes <sherlock656@gmail.com>
2026-02-08 11:57:14 -0700

    implement light/dark theme, add other settings elements

--- html/src/pages/Settings/index.jsx
+++ html/src/pages/Settings/index.jsx
@@ -14,7 +14,6 @@ export function Settings() {
         const fetchData = async () => {
             try {
                 const response = await fetch('/api/settings');
-                console.log(response);
                 if (!response.ok) {
                 throw new Error('Network response was not ok');
                 }
@@ -42,11 +41,99 @@ export function Settings() {
         </Overlay>
     }
 
+    function setTheme(mode) {
+        switch (mode) {
+            case 'schedule':
+                localStorage.removeItem('theme');
+                // setThemeBySchedule();
+                break;
+            case 'light':
+                localStorage.theme = 'light';
+                document.documentElement.classList.remove('dark');
+                break;
+            case 'dark':
+                localStorage.theme = 'dark';
+                document.documentElement.classList.add('dark');
+                break;
+            default:
+                break;
+        }
+    }
+
     return (
         <>
             <Header back={true} title="Settings" />
             <main>
-                <span>Settings Coming Soon</span>
+                <h2 className="font-bold text-2xl mb-2">Theme</h2>
+                <div className="grid grid-cols-3 border border-dark-blue dark:border-white rounded-lg mb-8">
+                    <button
+                        className="border-r border-dark-blue dark:border-white"
+                        onClick={() => setTheme('schedule')}
+                    >
+                        <div className="py-3 flex flex-col items-center gap-2">
+                            <i className="fa-solid fa-clock text-3xl"></i>
+			                <span className="font-bold">Schedule</span>
+                        </div>
+                    </button>
+                    <button
+                        className="border-r border-dark-blue dark:border-white"
+                        onClick={() => setTheme('light')}
+                    >
+                        <div className="py-3 flex flex-col items-center gap-2">
+                            <i className="fa-solid fa-sun text-3xl"></i>
+			                <span className="font-bold">Light</span>
+                        </div>
+                    </button>
+                    <button
+                        className=""
+                        onClick={() => setTheme('dark')}
+                    >
+                        <div className="py-3 flex flex-col items-center gap-2">
+                            <i className="fa-solid fa-moon text-3xl"></i>
+			                <span className="font-bold">Dark</span>
+                        </div>
+                    </button>
+                </div>
+
+                <h2 className="font-bold text-2xl mb-2">Theme Schedule</h2>
+                <div className="flex gap-3 mb-8">
+                    <div className="flex flex-col">
+                        <span className="font-bold text-lg">Set Light @</span>
+                        <input type="time" className="border rounded-lg p-2"></input>
+                    </div>
+                    <div className="flex flex-col">
+                        <span className="font-bold text-lg">Set Dark @</span>
+                        <input type="time" className="border rounded-lg p-2"></input>
+                    </div>
+                </div>
+
+                <h2 className="font-bold text-2xl mb-2">Brightness</h2>
+                <div className="flex gap-3 mb-8">
+                    <input type="range" className="w-full"
+                        min="0" max="100" value="50"></input>
+                </div>
+
+                <h2 className="font-bold text-2xl mb-2">Brightness Schedule</h2>
+                <div className="flex flex-col gap-3 mb-8">
+                    <div className="flex gap-2">
+                        <input type="checkbox"></input>
+                        <span className="font-bold text-lg">Use Schedule</span>
+                    </div>
+                    <div className="flex gap-3">
+                        <div className="flex flex-col">
+                            <span className="font-bold text-lg">Set Bright @</span>
+                            <input type="time" className="border rounded-lg p-2"></input>
+                            <input type="number" className="border rounded-lg p-2"
+                                min="0" max="100" step="1"></input>
+                        </div>
+                        <div className="flex flex-col">
+                            <span className="font-bold text-lg">Set Dim @</span>
+                            <input type="time" className="border rounded-lg p-2"></input>
+                            <input type="number" className="border rounded-lg p-2"
+                                min="0" max="100" step="1"></input>
+                        </div>
+                    </div>
+                </div>
             </main>
             <Footer />
         </>
